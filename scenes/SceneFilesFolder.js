46929427bc579d826314bef92e9f97f6dd4ed19c
toast <toastts@icloud.com>
2021-02-05 17:09:45 -0700

    working privacy filter and buggy filetype filter

--- scenes/SceneFilesFolder.js
+++ scenes/SceneFilesFolder.js
@@ -1,11 +1,16 @@
 import * as React from "react";
+import * as Constants from "~/common/constants";
 import * as SVG from "~/common/svg";
 import * as Events from "~/common/custom-events";
 
+import { css } from "@emotion/react";
+import { PopoverNavigation } from "~/components/system/components/PopoverNavigation";
 import { ButtonPrimary } from "~/components/system/components/Buttons";
 import { FileTypeGroup } from "~/components/core/FileTypeIcon";
 import { TabGroup, PrimaryTabGroup, SecondaryTabGroup } from "~/components/core/TabGroup";
+import { CheckBox } from "~/components/system/components/CheckBox.js";
 import { GlobalCarousel } from "~/components/system/components/GlobalCarousel";
+import { getPublicAndPrivateFiles } from "~/common/utilities";
 
 import ScenePage from "~/components/core/ScenePage";
 import DataView from "~/components/core/DataView";
@@ -15,9 +20,87 @@ import EmptyState from "~/components/core/EmptyState";
 
 const POLLING_INTERVAL = 10000;
 
+const STYLES_FILTERS_CONTAINER = css`
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: flex-start;
+`;
+
+const STYLES_FILTER_BUTTON = css`
+  padding: 11px;
+  border-radius: 4px;
+  border: 1px solid ${Constants.system.bgGray};
+  color: ${Constants.system.textGray};
+  margin-right: 8px;
+  display: flex;
+  align-items: center;
+  font-size: ${Constants.typescale.lvlN1};
+  cursor: pointer;
+  letter-spacing: -0.1px;
+`;
+
+const STYLES_FILETYPE_TOOLTIP = css`
+  border: 1px solid #f2f2f2;
+  background-color: ${Constants.system.white};
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  align-items: flex-start;
+  justify-content: space-around;
+  box-shadow: 0px 8px 24px rgba(178, 178, 178, 0.2);
+  width: 275px;
+  height: auto;
+  position: absolute;
+  top: 530px;
+  right: 60px;
+  z-index: ${Constants.zindex.tooltip};
+`;
+
 export default class SceneFilesFolder extends React.Component {
   state = {
     view: 0,
+    scopeTooltip: false,
+    filetypeTooltip: false,
+    filters: {},
+    files: this.props.viewer.library[0].children,
+  };
+
+  _handleScopeTooltip = () => {
+    this.setState({ scopeTooltip: !this.state.scopeTooltip });
+  };
+
+  _handleFiletypeTooltip = () => {
+    this.setState({ filetypeTooltip: !this.state.filetypeTooltip });
+  };
+
+  _handleScopeFilter = (filter) => {
+    let viewer = this.props.viewer;
+    let filtered = getPublicAndPrivateFiles({ viewer });
+    console.log(filtered.publicFiles);
+    console.log(filtered.privateFiles);
+
+    if (filter === "all") {
+      this.setState({ files: this.props.viewer.library[0]?.children });
+    } else if (filter === "public") {
+      this.setState({ files: filtered.publicFiles });
+    } else if (filter === "private") {
+      this.setState({ files: filtered.privateFiles });
+    } else {
+      console.log("this is the worst possible scenario");
+    }
+  };
+
+  _handleFiletypeFilter = ({ type, subtype }) => {
+    const key = `${type}/${subtype}`;
+    this.setState((prevState) => ({
+      filters: {
+        ...prevState.filters,
+        [key]: !prevState.filters[key],
+      },
+    }));
   };
 
   componentDidMount = () => {
@@ -28,6 +111,7 @@ export default class SceneFilesFolder extends React.Component {
         index = page.index;
       } else {
         let library = this.props.viewer.library[0]?.children || [];
+        this.setState({ files: library });
         for (let i = 0; i < library.length; i++) {
           let obj = library[i];
           if ((obj.cid && obj.cid === page?.cid) || (obj.id && obj.id === page?.fileId)) {
@@ -117,11 +201,132 @@ export default class SceneFilesFolder extends React.Component {
             </ButtonPrimary>
           }
         />
-        {this.props.viewer.library[0].children && this.props.viewer.library[0].children.length ? (
+        <div css={STYLES_FILTERS_CONTAINER} style={{ postion: "absolute" }}>
+          <div
+            css={STYLES_FILTER_BUTTON}
+            onClick={this._handleFiletypeTooltip}
+            style={{
+              right: 42,
+              top: 530,
+              borderColor: Constants.system.bgGray,
+              color: Constants.system.textGray,
+              width: 70,
+            }}
+          >
+            filetype
+            {this.state.filetypeTooltip ? (
+              <div css={STYLES_FILETYPE_TOOLTIP}>
+                <CheckBox
+                  name="mp4"
+                  value={false}
+                  style={{ padding: 5 }}
+                  onChange={this._handleFiletypeFilter(Constants.filetypes.mp4)}
+                >
+                  <strong>mp4</strong>
+                </CheckBox>
+                <CheckBox
+                  name="mp3"
+                  value={false}
+                  style={{ padding: 5 }}
+                  onChange={this._handleFiletypeFilter(Constants.filetypes.mp3)}
+                >
+                  <strong>mp3</strong>
+                </CheckBox>
+                <CheckBox
+                  name="epub"
+                  value={false}
+                  style={{ padding: 5 }}
+                  onChange={this._handleFiletypeFilter(Constants.filetypes.epub)}
+                >
+                  <strong>epub</strong>
+                </CheckBox>
+                <CheckBox
+                  name="pdf"
+                  value={false}
+                  style={{ padding: 5 }}
+                  onChange={this._handleFiletypeFilter(Constants.filetypes.pdf)}
+                >
+                  <strong>pdf</strong>
+                </CheckBox>
+                <CheckBox
+                  name="png"
+                  value={false}
+                  style={{ padding: 5 }}
+                  onChange={this._handleFiletypeFilter(Constants.filetypes.png)}
+                >
+                  <strong>png</strong>
+                </CheckBox>
+                <CheckBox
+                  name="jpg"
+                  value={false}
+                  style={{ padding: 5 }}
+                  onChange={this._handleFiletypeFilter(Constants.filetypes.jpg)}
+                >
+                  <strong>jpg</strong>
+                </CheckBox>
+              </div>
+            ) : null}
+          </div>
+          <div css={STYLES_FILTER_BUTTON} onClick={this._handleScopeTooltip}>
+            privacy
+            {this.state.scopeTooltip ? (
+              <PopoverNavigation
+                style={{
+                  right: 42,
+                  top: 530,
+                  borderColor: Constants.system.bgGray,
+                  color: Constants.system.textGray,
+                  width: 100,
+                }}
+                navigation={[
+                  {
+                    text: (
+                      <span
+                        style={{
+                          color: this.state.privacy === "all" ? Constants.system.brand : null,
+                        }}
+                      >
+                        All
+                      </span>
+                    ),
+                    onClick: () => this._handleScopeFilter("all"),
+                  },
+                  {
+                    text: (
+                      <span
+                        style={{
+                          color:
+                            this.state.privacy === "public" ? Constants.system.brand : "inherit",
+                        }}
+                      >
+                        public
+                      </span>
+                    ),
+                    onClick: () => this._handleScopeFilter("public"),
+                  },
+                  {
+                    text: (
+                      <span
+                        style={{
+                          color:
+                            this.state.privacy === "private" ? Constants.system.brand : "inherit",
+                        }}
+                      >
+                        private
+                      </span>
+                    ),
+                    onClick: () => this._handleScopeFilter("private"),
+                  },
+                ]}
+              />
+            ) : null}
+          </div>
+        </div>
+        {this.state.files?.length ? (
           <DataView
             onAction={this.props.onAction}
             viewer={this.props.viewer}
-            items={this.props.viewer.library[0].children}
+            items={this.state.files}
             onUpdateViewer={this.props.onUpdateViewer}
             view={this.state.view}
           />
